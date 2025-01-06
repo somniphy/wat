bad7da9a3489254e10c0e75c634f6313a169d172
chan <penalescjay@gmail.com>
2025-01-06 23:59:15 +0800

    changes

--- app/home/cta.tsx
+++ app/home/cta.tsx
@@ -3,7 +3,19 @@ import { ArrowUpRight } from "lucide-react";
 export function Cta() {
   return (
     <div className="py-12">
-      <h1 className="text-7xl text-lime-400 font-bold">Need website?</h1>
+      <h4 className="text-zinc-50 text-md md:text-2xl">Christian Penales<span className="text-lime-400">.</span></h4>
+      <h1 className="text-lime-400 text-5xl md:text-8xl font-bold">
+        Web Developer
+      </h1>
+      <h1 className="text-lime-400 text-5xl md:text-8xl font-bold">
+        Web Developer
+      </h1>
+      <h1 className="text-lime-400 text-5xl md:text-8xl font-bold">
+        Web Developer
+      </h1>
+      <h1 className="text-lime-400 text-5xl md:text-8xl font-bold">
+        Web Developer
+      </h1>
     </div>
   );
 }

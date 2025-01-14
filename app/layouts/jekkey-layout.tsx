930a4cf7d9937ef3ab6df18c2703cf4b4674ddd4
chan <penalescjay@gmail.com>
2025-01-14 23:45:33 +0800

    im so fucking done

--- app/layouts/jekkey-layout.tsx
+++ app/layouts/jekkey-layout.tsx
@@ -3,21 +3,21 @@ import { Link, Outlet } from "react-router";
 export default function JekkeyLayout() {
   return (
     <div>
-      <header className="h-[88px] px-[32px] flex justify-between items-center bg-[#090909]">
-        <Link to="/jekkey" className="text-white text-[20px] font-bold">
+      <header className="h-[88px] px-[32px] flex justify-between items-center bg-[#090909] w-screen max-w-[1440px]">
+        <Link to="/jekkey" className="text-white text-md font-bold">
           JEKKEY®
         </Link>
-        <p className="text-white uppercase text-[20px]">
+        <p className="text-white uppercase text-md">
           ARCHITECTURE PHOTOGRAPHY PORTFOLIO
         </p>
         <nav className="flex gap-[48px]">
-          <Link to="jekkey" className="text-white uppercase text-[20px]">
+          <Link to="jekkey" className="text-white uppercase text-md">
             Gallery
           </Link>
-          <Link to="jekkey" className="text-white uppercase text-[20px]">
+          <Link to="jekkey" className="text-white uppercase text-md">
             About
           </Link>
-          <Link to="jekkey" className="text-white uppercase text-[20px]">
+          <Link to="jekkey" className="text-white uppercase text-md">
             Contact
           </Link>
         </nav>
--- app/routes/jekkey/jekkey.tsx
+++ app/routes/jekkey/jekkey.tsx
@@ -5,16 +5,18 @@ import Second from "app/assets/jekkey/second.jpg";
 import Third from "app/assets/jekkey/third.jpg";
 export default function JekkeyHome() {
   return (
-    <main className="min-h-screen bg-[#1E1E1E] w-[1440px]">
+    <main className="min-h-screen bg-[#1E1E1E] w-screen max-w-[1440px]">
       <div className="grid grid-cols-3">
-        <img
-          src={Main}
-          alt="Main"
-          className="saturate-0 w-[857px] h-[556.36px] col-span-2"
-          width="857"
-          height="556.36"
-        />
-        <div className="w-max">
+        <div className="col-span-2 w-[857px] h-[556.36px]">
+          <img
+            src={Main}
+            alt="Main"
+            className="saturate-0 w-fit h-fit"
+            width="857"
+            height="556.36"
+          />
+        </div>
+        <div className="">
           <img
             src={First}
             alt="First"
